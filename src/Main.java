bec9607e7a7b2da83bb046df3163ac673abf3c49
mmorillo <matheus.morillo@nescara.com>
2019-07-23 10:53:58 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import java.io.IOException;
 import java.util.Timer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         Timer timer = new Timer();
         timer.schedule(new Scheduler(), 0, getMilis(30));
 
